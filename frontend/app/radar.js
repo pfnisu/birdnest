3653ebf32ae30daffcfaa76a5c1a8466dfdb7772
Niko Suoniemi <niko@tamperelainen.org>
2022-12-19 18:08:07 +0200

    Improve radar style

--- frontend/app/radar.js
+++ frontend/app/radar.js
@@ -6,22 +6,24 @@ export function Radar(root) {
     // Draw a dot representing object on radar
     this.dot = (x, y, name = null, opaque = true, radius = 4) => {
         const ctx = this.canvas.getContext('2d')
-        ctx.fillStyle = 'black'
         ctx.beginPath()
         ctx.arc(x, y, radius, 0, 360)
         opaque ? ctx.fill() : ctx.stroke()
-        ctx.font = '14px sans-serif'
-        if (name) ctx.fillText(name, x + radius + 2, y + 4)
+        ctx.font = '20px sans-serif'
+        if (name) ctx.fillText(name, x + radius + 2, y + 6)
     }
     this.compose = async () => {
         let json = await request('api/coords')
-        this.tree.innerHTML = '<h1>Drone radar</h1><canvas></canvas>'
+        this.tree.innerHTML =
+            `<h1>Drone radar</h1>
+            <p>Closest position of every detected drone</p>
+            <canvas></canvas>`
         this.canvas = this.tree.querySelector('canvas')
-        // Scale canvas 2x for better resolution
+        // Scale canvas 2x for sensible resolution
         this.canvas.height = this.canvas.width = 1000
-        this.dot(500, 500, 'Monadikuikka')
         this.dot(500, 500, '100m', false, 498)
         this.dot(500, 500, '50m', false, 250)
+        this.dot(500, 500, 'Monadikuikka')
         for (const pilot of json) {
             this.dot((pilot.x - 150000) / 200, (pilot.y - 150000) / 200, pilot.name)
         }
