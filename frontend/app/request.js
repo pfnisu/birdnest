e49899262fd03bce5037a59b29be2d5dd61c1a62
Niko Suoniemi <niko@tamperelainen.org>
2022-12-20 15:36:33 +0200

    Simplify request.http

--- frontend/app/request.js
+++ frontend/app/request.js
@@ -8,17 +8,14 @@ export let request = {
         return match?.length > 1 ? match[1] : null
     },
     // Fetch wrapper
-    // TODO remove unneeded feats
-    http: async (resource, method = 'GET', data = null) => {
+    http: async (resource, method = 'GET') => {
         try {
             let resp = await fetch(resource, {
                 method: method,
                 headers: { 'Content-type': 'application/json' },
-                body: data && JSON.stringify(data)
             })
             // Parse as json if expected, else return bool
-            if (resp.ok && resp.status != 204 &&
-                (method === 'GET' || method === 'POST')) return await resp.json()
+            if (resp.ok && resp.status != 204) return await resp.json()
             return resp.ok
         } catch(e) {
             console.log(`${method} request failed`)
