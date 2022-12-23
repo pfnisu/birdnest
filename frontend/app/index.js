8b0c9c486efa859d78163ebd893b5142ec630717
Niko Suoniemi <niko@tamperelainen.org>
2022-12-23 22:07:36 +0200

    Simplify view logic

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -3,8 +3,6 @@ import {Radar} from './radar.js'
 import {Info} from './info.js'
 import {request} from './request.js'
 
-const site = 'Birdnest'
-
 // Define state objects for views
 const main = document.querySelector('main')
 const views = [new List(main), new Radar(main), new Info(main)]
@@ -15,15 +13,14 @@ const nav = document.querySelector('nav')
 nav.innerHTML = tabs.reduce((cat, el) => `${cat}<a>${el}</a>`, '')
 
 // Set view to activated tab 
-const setView = async (ev) => {
-    views.forEach(view => view.stop())
-    const nav = document.querySelectorAll('nav a')
-    nav.forEach(el => el.className = '')
+const setView = (ev) => {
+    for (const v of views) v.stop()
+    for (const c of nav.children) c.className = ''
     const index = ev
         ? tabs.indexOf(ev.target.textContent)
         : request.cookie('tab') ?? 0
-    nav[index].className += 'active'
-    document.title = `${site} - ${views[index].title}`
+    nav.children[index].className = 'active'
+    document.title = `Birdnest - ${views[index].title}`
     request.interval = request.cookie('interval') ?? 10000
     views[index].start(request.interval)
     request.cookie('tab', index)
--- frontend/app/view.js
+++ frontend/app/view.js
@@ -1,15 +1,14 @@
 // View template
 export function View(target, root, title = '', live = true) {
-    target.root = root
     target.title = title
     target.tree = document.createElement('div')
     // Call from target to replace root tree
     this.compose = () => {
-        target.root.replaceChildren(target.tree)
+        root.replaceChildren(target.tree)
     }
     // Reload logic is only spawned if view is live and visible
-    target.start = async (interval) => {
-        await target.compose()
+    target.start = (interval) => {
+        target.compose()
         if (live && !target.id)
             target.id = setInterval(() => {
                 if (document.visibilityState === 'visible') target.compose()
